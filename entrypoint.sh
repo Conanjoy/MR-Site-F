68b71658295d98971ed900a26779eb047d368f19
Conanjoy <119912899+Conanjoy@users.noreply.github.com>
2023-02-12 20:42:25 +0530

    Update Files

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 env >> /etc/environment
 
-nohup python3 keep_alive.py > /proc/1/fd/1 2>/proc/1/fd/2 &
+python3 keep_alive.py > /proc/1/fd/1 2>/proc/1/fd/2 &
 
-cron -f
+cron -f &
+echo "App is Running..."
