7d135ecdaf7788a24150f9c611ba38af1ed287eb
Conanjoy <119912899+Conanjoy@users.noreply.github.com>
2023-02-12 23:20:15 +0530

    Update Files

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 env >> /etc/environment
 
-nohup python3 keep_alive.py > /proc/1/fd/1 2>/proc/1/fd/2 &
+nohup gunicorn keep_alive:app --bind 0.0.0.0:8080 --access-logfile - --error-logfile - &
 
 cron -f
--- keep_alive.py
+++ keep_alive.py
@@ -1,18 +1,10 @@
 from flask import Flask
-from threading import Thread
 
-app = Flask('')
+app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "App is Running..."
 
-def run():
-    app.run(host = '0.0.0.0', port = 8080)
-
-def keep_alive():
-    t = Thread(target = run)
-    t.start()
-
 if __name__ == "__main__":
-    keep_alive()
+    app.run()
