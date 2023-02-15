f8662547b36b3ebb19a9d86973524606555d9a39
Conanjoy <119912899+Conanjoy@users.noreply.github.com>
2023-02-16 02:49:20 +0530

    Update Files

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-env >> /etc/environment
 
-nohup gunicorn keep_alive:app --bind 0.0.0.0:8080 --access-logfile - --error-logfile - &
+nohup gunicorn keep_alive:app --bind 0.0.0.0:8080 &
 
-python3 ms_rewards_farmer.py --session --headless --fast --telegram "${TOKEN}" "${CHAT_ID}" &
+python3 ms_rewards_farmer.py --session --headless --fast --telegram "${TOKEN}" "${CHAT_ID}" &&
 
-cron -f
+python3 ms_rewards_farmer.py --everyday --start-at 04:00 --session --headless --fast --telegram "${TOKEN}" "${CHAT_ID}"
+
